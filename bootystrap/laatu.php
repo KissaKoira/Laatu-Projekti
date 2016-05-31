1d7379d1507214afa25ba0592551ddef5d03897d
Riqqeloff <riku-hanninen@hotmail.fi>
2016-05-31 11:38:36 +0300

    Alotus
    Teimme pohjaa :)))

---
+++ bootystrap/laatu.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style>
+.header{
+  height: 110px;
+  width:1000px;
+  background-color: #257425;
+}
+.kissa{
+  box-shadow: 10px 10px 5px grey;
+  height: 660px;
+  width:1000px;
+  margin: 0 auto;
+}
+
+
+
+    </style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Laatu</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="kissa">
+    <div  style="margin: 0 auto" class="header"></div>
+    <div id="carousel-example-generic" class="carousel slide" style="background-color: red; width: 1000px; height: 550px; margin: 0 auto" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="7"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="8"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="9"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="10"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="11"></li>
+
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <p>Insert XML anal here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+    <div class="item">
+      <p>Insert XML  here</p>
+      <div class="carousel-caption">
+        Kissa
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
