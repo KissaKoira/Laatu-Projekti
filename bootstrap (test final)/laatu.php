5b9cb9734d029e5dd0c720e17ac652367478b477
Riqqeloff <riku-hanninen@hotmail.fi>
2016-06-10 13:16:02 +0300

    poll, etusivu,
    poll, etusivu,

--- bootstrap (test final)/laatu.php
+++ bootstrap (test final)/laatu.php
@@ -40,96 +40,150 @@
 </head>
 <body>
 
-  <div class="varjo">
-      <div class="header"></div>
+<div id="wrapper">
+  <div id="navbaar"></div>
 
-      <div  id="carousel-example-generic" class="carousel slide" data-interval="false" style=" background-color: ;width: 1000px;height: 550px;  margin: 0 auto;" >
+      <div class="header"><img src="pics/esedu_logo.png" style="float: left;"></img>
+        <a id="header_text1">Etelä-Savon ammattiopisto</a><br>
+        <a id="header_text2">South Savo Vocational College</a>
+      </div>
+
+       <div id='carousel-custom' class="carousel slide" data-interval="false" >
 
         <!-- Sivujen indikaattorit -->
-        <ol class="carousel-indicators indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-custom" data-slide-to="0" class="active"><img src="pics/123.png"/></li>
+          <li data-target="#carousel-custom"  data-slide-to="1"><img src="pics/123.png"/></li>
+          <li data-target="#carousel-custom" data-slide-to="2"><img src="pics/123.png"/></li>
         </ol>
 
         <!-- Sivujen wrapperit -->
         <div class="carousel-inner" role="listbox">
 
-          <div class="item active" style="padding: 0px 130px 0px 130px;">
-            <div id="accordion">
-              <h3>Section 1</h3>
-              <div>
-                <p>
-                Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
-                ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
-                amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
-                odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
-                </p>
-              </div>
-              <h3>Section 2</h3>
-              <div>
-                <p>
-                Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
-                purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
-                velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
-                suscipit faucibus urna.
-                </p>
-              </div>
-              <h3>Section 3</h3>
-              <div>
-                <p>
-                Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
-                Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
-                ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
-                lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
-                </p>
-                <ul>
-                  <li>List item one</li>
-                  <li>List item two</li>
-                  <li>List item three</li>
-                </ul>
-              </div>
-              <h3>Section 4</h3>
-              <div>
-                <p>
-                Cras dictum. Pellentesque habitant morbi tristique senectus et netus
-                et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
-                faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
-                mauris vel est.
-                </p>
-                <p>
-                Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-                inceptos himenaeos.
-                </p>
-              </div>
-            </div>
+          <div class="item active">
+            <div class="rightside">
+              <img src="pics/etusivu.png"/>
+          </div>
+          <div class="leftside">
+            <h1>Laadunhallinnasta ja laadun kehityksestä Esedulaisille</h1>
           </div>
 
-          <div class="item">
-            <p>Sivu 2</p>
-          </div>
-
-          <div class="item">
-            <p>Sivu 3</p>
-          </div>
 
         </div>
+        <div class="item">
+            <div class="rightside">
+              <div id="accordion">
+            <h3>Section 1</h3>
+            <div>
+              <p>
+              Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+              ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+              amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+              odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+              </p>
+            </div>
+            <h3>Section 2</h3>
+            <div>
+              <p>
+              Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+              purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+              velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+              suscipit faucibus urna.
+              </p>
+            </div>
+            <h3>Section 3</h3>
+            <div>
+              <p>
+              Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+              Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+              ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+              lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+              </p>
+              <ul>
+                <li>List item one</li>
+                <li>List item two</li>
+                <li>List item three</li>
+              </ul>
+            </div>
+            <h3>Section 4</h3>
+            <div>
+              <p>
+              Cras dictum. Pellentesque habitant morbi tristique senectus et netus
+              et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
+              faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
+              mauris vel est.
+              </p>
+              <p>
+              Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
+              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+              inceptos himenaeos.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="leftside">
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+        </div>
+        </div>
+
+        <div class="item">
+          <div class="rightside">
+            <img src="pics/kysymysmerkki.png"/>
+          </div>
+          <div class="leftside">
+            <form method="post" action="send.php">
+              <h2> Totta vai Tarua? </h2>
+              <div id="vastauswrapper">
+              <p id="vastaus1">Totta ja Tarua</p>
+              <p id="vastaus1">Totta</p>
+              <p id="vastaus1">Tarua</p>
+
+              </div>
+              <div id="vastauswrapper">
+              <p id="kysymys">1. Hyvin suunniteltu on puoliksi tehty</p>
+              <fieldset id="group1">
+              <input id="vastaus" type="radio" name="group1">
+              <input id="vastaus" type="radio" name="group1">
+              <input id="vastaus" type="radio" name="group1"> <br></fieldset>
+            </div>
+            <div id="vastauswrapper">
+              <p id="kysymys">1. Hyvin suunniteltu on puoliksi tehty</p>
+              <fieldset id="group2">
+              <input id="vastaus" type="radio" name="group2">
+              <input id="vastaus" type="radio" name="group2">
+              <input id="vastaus" type="radio" name="group2"> <br></fieldset>
+            </div>
+            <div id="vastauswrapper">
+              <p id="kysymys">1. Hyvin suunniteltu on puoliksi tehty</p>
+              <fieldset id="group3">
+              <input id="vastaus" type="radio" name="group3">
+              <input id="vastaus" type="radio" name="group3">
+              <input id="vastaus" type="radio" name="group3"> <br></fieldset>
+            </div>
+            <div id="vastauswrapper">
+              <p id="kysymys">1. Hyvin suunniteltu on puoliksi tehty</p>
+              <fieldset id="group4">
+              <input id="vastaus" type="radio"  name="group4">
+              <input id="vastaus" type="radio"  name="group4">
+              <input id="vastaus" type="radio"  name="group4"> <br></fieldset>
+            </div>
+            <input type="submit" name="Lähetä">
+          </form>
+          </div>
+        </div>
+
       </div>
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-custom" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-custom" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-
-
-
+</div>
 
   <!-- lisätyt linkit (2) -->
   <script src="js/bootstrap.min.js"></script>
